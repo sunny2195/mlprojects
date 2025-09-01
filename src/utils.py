3c5ea6e97bd324650718e5dec5f64fadac36b6f3
sunny2195 <dheeraj2195@gmail.com>
2025-09-02 01:21:14 +0530

    Hyperparameter

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from src.logger import logging
 import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -20,10 +21,14 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models,params):
     try:
         report = {}
-        for i, (name, model) in enumerate(models.items()):
+        for name, model in models.items():
+            if name in params and params[name]:
+                gs = GridSearchCV(model, params[name], cv=3)
+                gs.fit(X_train, y_train)
+                model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
